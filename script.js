d8d0828a771580d42412fa361e22ead8741b5b43
ILoveGitHub122 <126015956+ILoveGitHub122@users.noreply.github.com>
2023-03-14 11:25:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("#message-form");
 const messageBoard = document.querySelector("#message-board");
+let messageCount = 0;
 
 form.addEventListener("submit", event => {
   event.preventDefault();
@@ -27,14 +28,28 @@ form.addEventListener("submit", event => {
   
   form.reset();
   
-  const postSection = document.querySelector('.post-section');
-  const premiumButton = document.createElement('button');
-   premiumButton.innerHTML = "Upgrade to Premium";
-   premiumButton.classList.add('premium-button');
-   postSection.appendChild(premiumButton);
+  messageCount++;
 
-   premiumButton.addEventListener('click', () => {
-   window.location.href = "premium.html";
-   });
+  if (messageCount === 1) {
+    const postSection = document.querySelector('.post-section');
+    const premiumButton = document.createElement('button');
+    premiumButton.innerHTML = "Kolla på reklam";
+    premiumButton.classList.add('premium-button');
+    postSection.appendChild(premiumButton);
 
+    const premiumText = document.createElement('p');
+    premiumText.innerHTML = 'Du måste kolla på en 0:30 sekunders annons för att skriva ett meddelande';
+    premiumText.classList.add('premium-text');
+    postSection.appendChild(premiumText);
+
+    premiumButton.addEventListener('click', () => {
+      const popup = window.open("https://www.google.com/adsense", "popup", "width=500,height=500");
+
+      setTimeout(() => {
+        popup.close();
+        postSection.removeChild(premiumButton);
+        postSection.removeChild(premiumText);
+      }, 30000);
+    });
+  }
 });
